aa15051dbc8b018561f3068240e4a799c875e535
Daksh Baweja <dakshbaweja20@gmail.com>
2026-01-17 02:31:27 +0530

    Update fuzzer.py

--- fuzzer.py
+++ fuzzer.py
@@ -2,15 +2,24 @@ import socket
 import random
 import string
 import time
+import sys
 
-target_ip = "127.0.0.1"
-target_port = 9999
+print("=========================================")
+print("Incremental Socket Fuzzer")
+print("=========================================")
+
+if len(sys.argv) != 5:
+    print("Usage: python socket_fuzzer.py <ip> <port> <start_size> <step>")
+    sys.exit(1)
+
+target_ip = sys.argv[1]
+target_port = int(sys.argv[2])
+payload_length = int(sys.argv[3])
+step = int(sys.argv[4])
 
 def generate_payload(length):
     letters = string.ascii_letters
     return "".join(random.choice(letters) for _ in range(length))
-
-payload_length = 100
 
 while True:
     try:
@@ -23,7 +32,7 @@ while True:
 
         print(f"Sent payload of size: {payload_length}")
 
-        payload_length += 100
+        payload_length += step
         time.sleep(0.5)
 
     except:
@@ -31,3 +40,7 @@ while True:
         print("Payload size:", payload_length)
         print("Payload used:", payload)
         break
+
+print("\n=========================================")
+print("Developed by sudo_0xksh")
+print("=========================================")
